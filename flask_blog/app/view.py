7f1f0b2933421d31d0e701041922ce2ab04aea33
Shkirmantsev <Shkirmantsev@gmail.com>
2019-01-16 10:48:32 +0100

    5_lesson blueprint posts|blogbprint

--- flask_blog/app/view.py
+++ flask_blog/app/view.py
@@ -1,9 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello world1'
+    name='Dimon'
+    return render_template('index.html',n=name)
 
-@app.route('/blog')
+""""@app.route('/blog')
 def index2():
-    return '<h1>Hello world2</h1>'
+    return '<h1>Hello world2</h1>'"""
